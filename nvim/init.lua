2ea4df30b19245e48c11ec2b190f49d1025838ff
Giovanni Cortes <giovanni@giovannicortes.com>
2022-12-08 11:27:42 -0600

    First nvim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+require('giovanni.plugins-setup')
+require('giovanni.core.options')
+require('giovanni.core.keymaps')
+require('giovanni.core.colorscheme')
+require('giovanni.plugins.comment')
+require('giovanni.plugins.nvim-tree')
